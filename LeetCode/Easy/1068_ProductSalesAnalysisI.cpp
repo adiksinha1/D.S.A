44f033761f2754d23509bdc41e3424106bed5567
adiksinha1 <adityaksinha1@gmail.com>
2026-06-27 18:04:00 +0530

    Sync LeetCode - 1068. Product Sales Analysis I (Easy)

--- LeetCode/Easy/1068_ProductSalesAnalysisI.cpp
+++ LeetCode/Easy/1068_ProductSalesAnalysisI.cpp
@@ -7,8 +7,5 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-# Write your MySQL query statement below
-select p.product_name , s.year , s.price
-from Sales as s
-Left Join Product as p
-ON s.product_id = p.product_id
+select P.product_name, S.year, S.price from Sales S inner join Product P
+ON S.product_id = P.product_id

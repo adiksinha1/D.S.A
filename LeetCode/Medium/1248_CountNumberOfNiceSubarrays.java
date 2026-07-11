75ef1e99a8f13b16d2d6e47f0920fd3b51d73bf4
adiksinha1 <adityaksinha1@gmail.com>
2026-07-11 14:59:25 +0530

    Sync LeetCode - 1248. Count Number of Nice Subarrays (Medium)

---
+++ LeetCode/Medium/1248_CountNumberOfNiceSubarrays.java
@@ -0,0 +1,22 @@
+/**
+ * Problem Link : https://leetcode.com/problems/count-number-of-nice-subarrays/
+ * Platform     : LeetCode
+ * Difficulty   : Medium
+ */
+
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+         return atMost(nums, k) - atMost(nums, k - 1);
+        
+    }
+     public int atMost(int[] nums, int k) {
+        int res = 0, i = 0, n = nums.length;
+        for (int j = 0; j < n; j++) {
+            k -= nums[j] % 2;
+            while (k < 0)
+                k += nums[i++] % 2;
+            res += j - i + 1;
+        }
+        return res;
+    }
+}

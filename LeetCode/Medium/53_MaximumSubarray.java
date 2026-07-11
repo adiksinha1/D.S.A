bb041bd937076e1fc7628f1b1b36f99335ab154b
adiksinha1 <adityaksinha1@gmail.com>
2026-07-11 13:59:18 +0530

    Sync LeetCode - 53. Maximum Subarray (Medium)

---
+++ LeetCode/Medium/53_MaximumSubarray.java
@@ -0,0 +1,21 @@
+/**
+ * Problem Link : https://leetcode.com/problems/maximum-subarray/
+ * Platform     : LeetCode
+ * Difficulty   : Medium
+ */
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++)
+        {
+        sum=sum+nums[i];
+        max=Math.max(max,sum);
+        if(sum<=0)
+        sum=0;
+        }
+        return max;
+        
+    }
+}

d33d94718b542beadf938e81a6aaac342ab56c81
adiksinha1 <adityaksinha1@gmail.com>
2026-07-15 23:46:40 +0530

    Sync LeetCode - 3658. GCD of Odd and Even Sums (Easy)

---
+++ LeetCode/Easy/3658_GCDOfOddAndEvenSums.java
@@ -0,0 +1,12 @@
+/**
+ * Problem Link : https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+        
+    }
+}

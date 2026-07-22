2b6fb1b6e58b38f2c2b91b317ef512ffb05b1fa0
adiksinha1 <adityaksinha1@gmail.com>
2026-07-22 14:57:38 +0530

    Sync LeetCode - 80. Remove Duplicates from Sorted Array II (Medium)

--- LeetCode/Medium/80_RemoveDuplicatesFromSortedArrayII.java
+++ LeetCode/Medium/80_RemoveDuplicatesFromSortedArrayII.java
@@ -6,27 +6,20 @@
 
 class Solution {
     public int removeDuplicates(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int n:nums)
-        {
-            map.put(n,map.getOrDefault(n,0)+1);
+       if (nums.length <= 2) {
+            return nums.length;
         }
-        int j=0;
-        int a;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(map.getOrDefault(nums[i],0)>=2)
-            {
-            a=map.getOrDefault(nums[i],0);
-            nums[j++]=nums[i];
-            nums[j++]=nums[i];
-            i+=a-1;
-            }
-            else
-            nums[j++]=nums[i];
 
+        int i = 2;
+
+        for (int j = 2; j < nums.length; j++) {
+            if (nums[j] != nums[i - 2]) {
+                nums[i] = nums[j];
+                i++;
+            }
         }
-        return j;
+
+        return i;
 
     }
 }

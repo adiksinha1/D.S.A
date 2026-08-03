bfe25632e55bce92a10263df5a18d6bbb8b8d527
adiksinha1 <adityaksinha1@gmail.com>
2026-08-03 15:26:16 +0530

    Time: 5 ms (98.25%) | Memory: 42.9 MB (12.87%) - LeetSync

--- 1276-closest-divisors/closest-divisors.java
+++ 1276-closest-divisors/closest-divisors.java
@@ -1,47 +1,20 @@
 class Solution {
     public int[] closestDivisors(int num) {
-        int g=num+1;
-        int g1=num+2;
-        int n=(int)Math.sqrt(num+1);
-        int m=(int)Math.sqrt(num+2);
-        int l=0;
-        int k=0;
-        int a[]=new int[2];
-        while(n>0)
-        {
-            if(g%n==0)
-            {
-                l=g/n;
-                break;
+        int numOne = num + 1;
+        int numTwo = num + 2;
 
+        for (int temp = (int) Math.sqrt(numTwo); temp > 0; temp--) {
+           
+            if (numOne % temp == 0) {
+                return new int[]{temp, numOne / temp};
+            } 
+
+            
+            if (numTwo % temp == 0) {
+                return new int[]{temp, numTwo / temp};
             }
-             n--;
+        }
 
-        }
-        int diff1=Math.abs(l-n);
-         while(m>0)
-        {
-            if(g1%m==0)
-            {
-                k=g1/m;
-                break;                
-
-            }
-             m--;
-
-        }
-        int diff2=Math.abs(k-m);
-        if(diff1<diff2)
-        {
-            a[0]=l;
-            a[1]=n;
-        }
-        else
-        {
-            a[0]=k;
-            a[1]=m;
-        }
-        return a;
-        
+        return new int[]{};
     }
 }

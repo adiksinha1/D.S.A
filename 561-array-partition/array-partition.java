49e05c81cc3e6812b3f32865f81d6a980d4b2b37
adiksinha1 <adityaksinha1@gmail.com>
2026-08-20 11:52:49 +0530

    Time: 18 ms (31.01%) | Memory: 49.6 MB (54.83%) - LeetSync

---
+++ 561-array-partition/array-partition.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int min=0;
+        int sum=0;
+        for(int i=0;i<nums.length;i+=2)
+        {
+            min=Math.min(nums[i],nums[i+1]);
+            sum+=min;
+        }
+        return sum;
+    }
+}

bc38d9ee9cdfe01f70fde0715a08e9a1e3080ca4
adiksinha1 <adityaksinha1@gmail.com>
2026-09-20 11:36:37 +0530

    Time: 8 ms (14.93%) | Memory: 46.3 MB (57.65%) - LeetSync

---
+++ 3644-minimum-positive-sum-subarray/minimum-positive-sum-subarray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumSumSubarray(List<Integer> nums, int l, int r) {
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<nums.size();i++)
+        {
+            int count=0;
+            int sum=0;
+            for(int j=i;j<nums.size();j++)
+            {
+                sum+=nums.get(j);
+                count++;
+                if(sum>0&&count>=l&&count<=r)
+                min=Math.min(min,sum);
+            }
+        }
+        if(min==Integer.MAX_VALUE)
+    return -1;
+    return min;
+        
+    }
+    
+}
